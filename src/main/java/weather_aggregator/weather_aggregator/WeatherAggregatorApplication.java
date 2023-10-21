bae13763d6be37314c220e69e2870fbd19699b03
tuckerdev <tuckerdev100@outlook.com>
2023-10-21 13:59:25 -0600

    first commit

---
+++ src/main/java/weather_aggregator/weather_aggregator/WeatherAggregatorApplication.java
@@ -0,0 +1,13 @@
+package weather_aggregator.weather_aggregator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherAggregatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAggregatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/weather_aggregator/weather_aggregator/WeatherAggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package weather_aggregator.weather_aggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherAggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
